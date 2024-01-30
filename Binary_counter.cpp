416d05ff51c0ad1aed7ee7c8448c2b0d98c5b043
Palla04 <128132131+Palla04@users.noreply.github.com>
2024-01-30 19:36:04 +0530

    Add files via upload

---
+++ Binary_counter.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int Increment(int A[],int k)
+{
+	int i=0;
+	for(i=k-1;i>=0;i--)
+	{
+		cout<<" "<<A[i];
+	}
+	cout<<endl;
+	
+	i=0;
+	while(i<k && A[i]==1)
+	{
+		A[i]=0;
+		i=i+1;
+	}
+	if(i<k)
+	{
+		A[i]=1;
+	}
+}
+int main()
+{
+	int size,count;
+	cout<<"Enter the size: ";
+	cin>>size;
+	
+	int A[100]={0};
+	for (count = 0; count < (1 << size); count++) {
+        Increment(A, size);
+    }
+
+	return 0;
+}
